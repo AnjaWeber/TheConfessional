cd498e3dbcb45e001ec77c85143fe8ae84ecba9d
AnjaWeber <92021165+AnjaWeber@users.noreply.github.com>
2023-07-20 23:18:19 +0200

    Entferne Test Dateien

--- node-thermal-printer-poc/dummy.js
+++
@@ -1,6 +0,0 @@
-const s =
-  "Mein Geheimnis ist, dass die Katzen das Wort Schmock für Papi gar nicht eingeführt haben sondern ich";
-const lines = s.match(/(.{1,48}(\s|$))\s*/g);
-lines.forEach((line) => {
-  console.log(line.trim());
-});
